71d610e8ff9146c4dd3cd132716b09eb9df67fd1
unknown <bni.sergio@gmail.com>
2022-11-25 19:56:54 +0100

    segunda ronda ejercicios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular-ejercicios-dos';
+  mensaje = '';
+
+  actualizar(t: any) {
+    this.mensaje = t + '(Se actualiza cada 10 segundos)';
+  }
+
+  @ViewChild('selector1') selector1: any;
+  @ViewChild('selector2') selector2: any;
+
+  fijarSelector1(valor: number) {
+    this.selector1.fijar(valor);
+  }
+
+  fijarSelector2(valor: number) {
+    this.selector2.fijar(valor);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CronometroComponent } from './cronometro/cronometro.component';
+import { SelectornumericoComponent } from './selectornumerico/selectornumerico.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CronometroComponent,
+    SelectornumericoComponent
   ],
   imports: [
     BrowserModule,
